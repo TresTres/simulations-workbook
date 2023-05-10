657fe73ae8a20323694ae3afc4f0842decdd7bb8
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2023-05-10 02:25:35 -0500

    add formatter

--- physics-simulations/physics_simulations/physical_laws.py
+++ physics-simulations/physics_simulations/physical_laws.py
@@ -1,21 +1,44 @@
 import logging
+
 NEWTON_G_CONST = 6.67430e-11
 
 
 logger = logging.getLogger(__name__)
 
-def newton_law_of_gravitation(mass1: float, mass2: float, distance: float, G: float = NEWTON_G_CONST) -> float:
+
+def kinematic_distance(
+    time: float, distance: float = 0, velocity: float = 0, acceleration: float = 0
+) -> float:
+    """
+    Calculates the distance travelled using the kinematic equation:
+    distance = initial_distance + initial_velocity * time + 0.5 * acceleration * time ** 2
+    Time is assumed to be in seconds, the other parameters can be expressed using consistent units,
+    e.g. meters, meters per second, meters per second squared.
+    """
+    try:
+        if any([time < 0, distance < 0]):
+            raise ValueError(
+                f"Time and distance must be positive. Time: {time}, distance: {distance}"
+            )
+        return distance + velocity * time + 0.5 * acceleration * time**2
+    except ValueError as e:
+        logger.error(e)
+        raise
+
+
+def newton_universal_gravitation(
+    mass1: float, mass2: float, distance: float, G: float = NEWTON_G_CONST
+) -> float:
     """
     Calculates the force of attraction between two masses in Newtons.
     Masses are assumed to be in kilograms and distance in meters.
     """
     try:
         if any([mass1 <= 0, mass2 <= 0, distance <= 0]):
-            raise ValueError(f"Masses and distance must be positive. Masses: {mass1}, {mass2}. Distance: {distance}")
+            raise ValueError(
+                f"Masses and distance must be positive. Masses: {mass1}, {mass2}. Distance: {distance}"
+            )
         return G * (mass1 * mass2) / (abs(distance) ** 2)
     except ValueError as e:
         logger.error(e)
         raise
-        
-        
-        
--- physics-simulations/tests/test_physical_laws.py
+++ physics-simulations/tests/test_physical_laws.py
@@ -1,10 +1,20 @@
 import pytest
 import physics_simulations.physical_laws as pl
 
+
 class TestPhysicalLaws:
-    
-    def test_newton_law_of_gravitation(self):
-        assert pl.newton_law_of_gravitation(1, 1, 1) == pl.NEWTON_G_CONST
-        assert pl.newton_law_of_gravitation(20, 10, 2) == pl.NEWTON_G_CONST * 50
+    def test_kinematic_distance(self):
+        assert pl.kinematic_distance(0) == 0
+        assert pl.kinematic_distance(1, 0, 0, 0) == 0
+        assert pl.kinematic_distance(1, 1, 0, 0) == 1
+        assert pl.kinematic_distance(1, 0, 1, 0) == 1
+        assert pl.kinematic_distance(1, 0, 0, 1) == 0.5
+        assert pl.kinematic_distance(1, 10, 20, 30) == 45
         with pytest.raises(ValueError):
-            pl.newton_law_of_gravitation(0, 1, 1)
+            pl.kinematic_distance(-1, 0, 0, 0)
+
+    def test_newton_gravitation(self):
+        assert pl.newton_universal_gravitation(1, 1, 1) == pl.NEWTON_G_CONST
+        assert pl.newton_universal_gravitation(20, 10, 2) == pl.NEWTON_G_CONST * 50
+        with pytest.raises(ValueError):
+            pl.newton_universal_gravitation(0, 1, 1)
