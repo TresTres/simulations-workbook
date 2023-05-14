0df58b6692e2ab1904a4d0e7df910c582d3239c3
Ez Ablaza <ezra.ablaza.inbox@gmail.com>
2023-05-13 20:57:01 -0500

    refactor laws to accept vector calculations

--- physics-simulations/physics_simulations/physical_laws.py
+++ physics-simulations/physics_simulations/physical_laws.py
@@ -1,14 +1,18 @@
 import logging
 
-NEWTON_G_CONST = 6.67430e-11
-
+from vector_math import Vec_3D
 
 logger = logging.getLogger(__name__)
 
 
+NEWTON_G_CONST = 6.67430e-11
+
 def kinematic_distance(
-    time: float, distance: float = 0, velocity: float = 0, acceleration: float = 0
-) -> float:
+    time: float,
+    distance: Vec_3D = Vec_3D.ZERO,
+    velocity: Vec_3D = Vec_3D.ZERO,
+    acceleration: Vec_3D = Vec_3D.ZERO,
+) -> Vec_3D:
     """
     Calculates the distance travelled using the kinematic equation:
     distance = initial_distance + initial_velocity * time + 0.5 * acceleration * time ** 2
@@ -17,9 +21,7 @@ def kinematic_distance(
     """
     try:
         if time < 0:
-            raise ValueError(
-                f"Time must be positive. Time: {time}"
-            )
+            raise ValueError(f"Time must be positive. Time: {time}")
         return distance + velocity * time + 0.5 * acceleration * time**2
     except ValueError as e:
         logger.error(e)
@@ -27,19 +29,16 @@ def kinematic_distance(
 
 
 def newton_universal_gravitation(
-    mass1: float, mass2: float, distance: float, G: float = NEWTON_G_CONST
-) -> float:
+    mass1: float, mass2: float, distance: Vec_3D, G: float = NEWTON_G_CONST
+) -> Vec_3D:
     """
     Calculates the force of attraction between two masses in Newtons.
-    Masses are assumed to be in kilograms and distance in meters.
-    Distance can be treated as a vector, but the magnitude is used.
+    Masses are assumed to be in kilograms and distances expressed in meters.
     """
     try:
-        if any([mass1 <= 0, mass2 <= 0]):
-            raise ValueError(
-                f"Masses must be positive. Masses: {mass1}, {mass2}"
-            )
-        return G * (mass1 * mass2) / (abs(distance) ** 2)
+        if any([mass1 <= 0, mass2 <= 0, distance.magnitude <= 0]):
+            raise ValueError(f"Masses and distance magnitude must be positive. Masses: {mass1}, {mass2}, Distance: {distance}")
+        return -1 * distance.unit_vector * G * (mass1 * mass2) / (distance.magnitude**2)
     except ValueError as e:
         logger.error(e)
         raise
